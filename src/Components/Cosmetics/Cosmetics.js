a1d07fbea2845a3f201c6351bc0d444e20e37fa6
soubj <sobujahmed124@gmail.com>
2023-04-11 23:07:14 +0600

    understanding basic export

--- src/Components/Cosmetics/Cosmetics.js
+++ src/Components/Cosmetics/Cosmetics.js
@@ -1,9 +1,14 @@
 import React from "react";
+import add from "../Utilities/Utilities";
 
 const Cosmetics = () => {
+  const first = 44;
+  const second = 33;
+  const total = add(first, second);
   return (
     <div>
       <h3>This is cosmetics component.</h3>
+      <h4>Total : {total}</h4>
     </div>
   );
 };
