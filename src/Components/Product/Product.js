e2dc9a236289e5a2206efc7b00ba6ea8fd19bda1
soubj <sobujahmed124@gmail.com>
2023-04-12 22:43:16 +0600

    onClick hander dynamic with arrow function

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -3,11 +3,20 @@ import "./Product.css";
 
 const Product = (props) => {
   const { index, name, age } = props.product;
+
+  const cart = (index) => {
+    console.log(index);
+  };
+  // this function is used to understand better.
+  const cartWithPara = () => cart(index);
+
   return (
     <div className="product">
       <h2>Person name: {name}</h2>
       <h3>Age : {age}</h3>
       <h4>ID :{index}</h4>
+      <button onClick={cartWithPara}>Add to cart</button>
+      <button onClick={() => cart(index)}>Add to cart: most used method</button>
     </div>
   );
 };
