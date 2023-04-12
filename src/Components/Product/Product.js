2c30833f1a8ffb52855a5695a44126c6c9c0e820
soubj <sobujahmed124@gmail.com>
2023-04-12 19:39:22 +0600

    created customized data and used it

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const Product = (props) => {
-  const { id, name, price } = props.product;
+  const { index, name, age } = props.product;
   return (
     <div>
-      <h2>Product name: {name}</h2>
-      <h3>Price : {price}</h3>
-      <h4>ID :{id}</h4>
+      <h2>Person name: {name}</h2>
+      <h3>Age : {age}</h3>
+      <h4>ID :{index}</h4>
     </div>
   );
 };
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -3,17 +3,47 @@ import Product from "../Product/Product";
 
 const Products = () => {
   const products = [
-    { id: 1, name: "sari", price: 5000 },
-    { id: 2, name: "punjabi", price: 2000 },
-    { id: 3, name: "dhuti", price: 1000 },
-    { id: 4, name: "shirt", price: 500 },
-    { id: 5, name: "pant", price: 800 },
+    {
+      index: 0,
+      age: 37,
+      name: "Vera York",
+    },
+    {
+      index: 1,
+      age: 34,
+      name: "Mitchell Lott",
+    },
+    {
+      index: 2,
+      age: 29,
+      name: "Ebony Duke",
+    },
+    {
+      index: 3,
+      age: 31,
+      name: "Rowe Vega",
+    },
+    {
+      index: 4,
+      age: 35,
+      name: "Janis Collins",
+    },
+    {
+      index: 5,
+      age: 21,
+      name: "Craig Wise",
+    },
+    {
+      index: 6,
+      age: 38,
+      name: "Penny Beasley",
+    },
   ];
   return (
     <div>
       <h1>This is products component.</h1>
       {products.map((product) => (
-        <Product product={product} key={product.id}></Product>
+        <Product product={product} key={product.index}></Product>
       ))}
     </div>
   );
