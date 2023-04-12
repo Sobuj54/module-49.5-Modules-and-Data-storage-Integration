76355468ed674a34d04294d2100d0f47c0877e76
soubj <sobujahmed124@gmail.com>
2023-04-12 19:44:59 +0600

    created my own json file and used fetch to get data

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,44 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Product from "../Product/Product";
 
 const Products = () => {
-  const products = [
-    {
-      index: 0,
-      age: 37,
-      name: "Vera York",
-    },
-    {
-      index: 1,
-      age: 34,
-      name: "Mitchell Lott",
-    },
-    {
-      index: 2,
-      age: 29,
-      name: "Ebony Duke",
-    },
-    {
-      index: 3,
-      age: 31,
-      name: "Rowe Vega",
-    },
-    {
-      index: 4,
-      age: 35,
-      name: "Janis Collins",
-    },
-    {
-      index: 5,
-      age: 21,
-      name: "Craig Wise",
-    },
-    {
-      index: 6,
-      age: 38,
-      name: "Penny Beasley",
-    },
-  ];
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
     <div>
       <h1>This is products component.</h1>
