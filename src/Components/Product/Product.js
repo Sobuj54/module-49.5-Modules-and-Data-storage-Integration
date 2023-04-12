8ab116499f3c4d46d026277cecb9123525887bd8
soubj <sobujahmed124@gmail.com>
2023-04-12 20:05:18 +0600

    added style

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Product.css";
 
 const Product = (props) => {
   const { index, name, age } = props.product;
   return (
-    <div>
+    <div className="product">
       <h2>Person name: {name}</h2>
       <h3>Age : {age}</h3>
       <h4>ID :{index}</h4>
